56bf50f053a59d74a5545236526cf6948af6e578
hyunoi <nhe0622@gmail.com>
2023-07-11 16:31:12 +0900

    20230711

--- 1920.py
+++ 1920.py
@@ -12,3 +12,27 @@ for i in mlist:
         print(1)
     else:
         print(0)
+
+#2
+nnum = int(input())
+nlist = list(map(int, sys.stdin.readline().split()))
+nlist.sort()
+mnum = int(input())
+mlist = list(map(int, sys.stdin.readline().split()))
+
+for num in mlist:
+    lo, hi = 0, nnum-1
+    temp = False
+    while lo <= hi:
+        mid = (lo+hi) // 2
+        if num == nlist[mid]:   # 찾은 경우
+            temp = True
+            print(1)
+            break
+        elif num > nlist[mid]:  # num 값이 mid 인덱스 값보다 큰 경우
+            lo = mid + 1        # 리스트의 오른쪽만 사용
+        else:                   # num 값이 mid 인덱스 값보다 작은 경우
+            hi = mid-1          # 리스트의 왼쪽만 사용
+          
+    if temp == False:
+        print(0)
